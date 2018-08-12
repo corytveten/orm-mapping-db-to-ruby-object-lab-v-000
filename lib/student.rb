1212992160676d33a7d57c156fe7b7ab93a5fc76
corytveten <corytveten@gmail.com>
2018-08-12 18:18:02 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -39,7 +39,15 @@ class Student
   end
 
   def count_all_students_in_grade_9
+    sql = <<-SQL
+      SELECT name
+      FROM students
+      WHERE grade = 9
+    SQL
 
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end
   end
 
   def save
